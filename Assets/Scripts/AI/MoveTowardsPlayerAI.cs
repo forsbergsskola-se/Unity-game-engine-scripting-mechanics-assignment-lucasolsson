367aa0e0fdbc63613b2a2c4e1a92b1149ab8cd32
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-02-03 22:59:58 +0100

    added player identifier that sticks to player

--- Assets/Scripts/AI/MoveTowardsPlayerAI.cs
+++ Assets/Scripts/AI/MoveTowardsPlayerAI.cs
@@ -6,10 +6,19 @@ public class MoveTowardsPlayerAI : MonoBehaviour
   [SerializeField] private CommandController commandContainer;
   [SerializeField] private Transform playerTransform;
 
+  private void Start()
+  {
+    // playerTransform = ((PlayerIdentifier) FindObjectOfType((typeof(PlayerIdentifier)))).transform; // Looks for a gameobject with componen PlayerIdentifier
+    playerTransform = FindObjectOfType<PlayerIdentifier>().transform; //Looks for a gameobject with the component PlayerIdentfier but with cleaner code.
+  }
+
   private void Update()
   {
     //to get a direction vector to a target: {Target:_Position} - {Origin_Position}
     var directionToPlayer = playerTransform.position - transform.position;
+    
+    directionToPlayer.Normalize();
+    
     //Get the horizontal part of our direction. 
     var horizontalDirection = directionToPlayer.x;
     //Pass along direction to command container
